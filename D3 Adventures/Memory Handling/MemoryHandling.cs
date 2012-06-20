3a1941b1d8fa8eed3a5290d6be3c4647625942fc
mandrakiane@gmail.com <mandrakiane@gmail.com@8f528425-eb94-3cc1-85a0-62ae32f851fc>
2012-06-20 23:48:49 +0000

    Real offset for injection should be SAFE (D3D.dll)

--- D3 Adventures/Memory Handling/MemoryHandling.cs
+++ D3 Adventures/Memory Handling/MemoryHandling.cs
@@ -49,7 +49,9 @@ namespace D3_Adventures.Memory_Handling
 
         public void Attach()
         {
-            this.Injector = new ASMExecutor(this, new IntPtr(0xc54580));
+            ProcessModule d3d = GetProcessModule("d3d9.dll");
+
+            this.Injector = new ASMExecutor(this, new IntPtr(d3d.BaseAddress.ToInt32() + 0x2279f));
         }
 
         public void CreateRemoteThread(uint address, IntPtr parameter)
